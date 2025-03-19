98d32f17ea448bc9127b073bf8d0d8087fb9edc8
todokr <s.tadokoro0317@gmail.com>
2025-03-20 01:32:12 +0900

    otel

--- api/src/middleware.ts
+++ api/src/middleware.ts
@@ -1,15 +1,9 @@
 import { createMiddleware } from "hono/factory";
 
-// TODO AuthN
-type LoginUser = {
-  userId: string;
-  tenantId: string;
-}
-export const authN = createMiddleware<{
-  Variables: {
-    loginUser?: LoginUser
-  }
-}>(async (c, next) => {
-  c.set('loginUser', {userId: "test_user", tenantId: "0000000000000000TENANT_001"})
-  await next()
+export const authN = createMiddleware(async (c, next) => {
+  c.set("loginUser", {
+    userId: "test_user",
+    tenantId: "0000000000000000TENANT_001",
+  });
+  await next();
 });
